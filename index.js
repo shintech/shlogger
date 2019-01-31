994c77ff47a48c4075f36a1d8aa4f9535b562724
Michael Prather <mprather1@gmail.com>
2019-01-30 20:58:36 -0600

    log directory as argument

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const winston = require('winston')
 const path = require('path')
 
 const NODE_ENV = process.env['NODE_ENV']
-const LOG_DIR = process.env['LOG_DIR'] || 'log'
 
-module.exports = function () {
+module.exports = function ({ directory = 'log' }) {
   let logger = winston.createLogger({
     level: 'info',
     format: winston.format.json()
@@ -12,13 +11,13 @@ module.exports = function () {
 
   if (NODE_ENV === 'test') {
     return logger.add(
-      new winston.transports.File({ filename: path.join(LOG_DIR, 'test.log'), level: 'info' })
+      new winston.transports.File({ filename: path.join(directory, 'test.log'), level: 'info' })
     )
   }
 
   if (NODE_ENV !== undefined) {
-    logger.add(new winston.transports.File({ filename: path.join(LOG_DIR, 'error.log'), level: 'error' }))
-    logger.add(new winston.transports.File({ filename: path.join(LOG_DIR, 'combined.log'), level: 'info' }))
+    logger.add(new winston.transports.File({ filename: path.join(directory, 'error.log'), level: 'error' }))
+    logger.add(new winston.transports.File({ filename: path.join(directory, 'combined.log'), level: 'info' }))
   }
 
   if (NODE_ENV !== 'production') {
